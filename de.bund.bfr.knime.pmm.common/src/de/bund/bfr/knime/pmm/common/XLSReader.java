745e4738db510a7f49b90b036a3d1b7948683f84
cthoens <christian.thoens@bfr.bund.de>
2012-10-26 17:32:49 +0200

    XLS Reader updated

--- de.bund.bfr.knime.pmm.common/src/de/bund/bfr/knime/pmm/common/XLSReader.java
+++ de.bund.bfr.knime.pmm.common/src/de/bund/bfr/knime/pmm/common/XLSReader.java
@@ -1,3 +1,36 @@
+/*******************************************************************************
+ * PMM-Lab © 2012, Federal Institute for Risk Assessment (BfR), Germany
+ * 
+ * PMM-Lab is a set of KNIME-Nodes and KNIME workflows running within the KNIME software plattform (http://www.knime.org.).
+ * 
+ * PMM-Lab © 2012, Federal Institute for Risk Assessment (BfR), Germany
+ * Contact: armin.weiser@bfr.bund.de or matthias.filter@bfr.bund.de 
+ * 
+ * Developers and contributors to the PMM-Lab project are 
+ * Jörgen Brandt (BfR)
+ * Armin A. Weiser (BfR)
+ * Matthias Filter (BfR)
+ * Alexander Falenski (BfR)
+ * Christian Thöns (BfR)
+ * Annemarie Käsbohrer (BfR)
+ * Bernd Appel (BfR)
+ * 
+ * PMM-Lab is a project under development. Contributions are welcome.
+ * 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package de.bund.bfr.knime.pmm.common;
 
 import java.io.File;
@@ -5,12 +38,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -21,40 +54,43 @@ import de.bund.bfr.knime.pmm.common.pmmtablemodel.TimeSeriesSchema;
 
 public class XLSReader {
 
+	private static String ID = "ID";
+
+	private static String[] STANDARD_COLUMNS = { ID,
+			TimeSeriesSchema.ATT_AGENTNAME, TimeSeriesSchema.ATT_MATRIXNAME,
+			TimeSeriesSchema.ATT_COMMENT, TimeSeriesSchema.ATT_TIME,
+			TimeSeriesSchema.ATT_LOGC, TimeSeriesSchema.ATT_TEMPERATURE,
+			TimeSeriesSchema.ATT_PH, TimeSeriesSchema.ATT_WATERACTIVITY };
+
 	private XLSReader() {
 	}
 
 	public static Map<String, KnimeTuple> getTuples(File file) throws Exception {
 		Map<String, KnimeTuple> tuples = new LinkedHashMap<String, KnimeTuple>();
 		InputStream inputStream = null;
+
 		if (file.exists()) {
 			inputStream = new FileInputStream(file);
 		} else {
 			try {
 				URL url = new URL(file.getPath());
+
 				inputStream = url.openStream();
 			} catch (Exception e) {
 				throw new FileNotFoundException("File not found");
 			}
 		}
+
 		Workbook wb = WorkbookFactory.create(inputStream);
 		Sheet sheet = wb.getSheetAt(0);
-		List<String> miscNames = getAllMiscNames(sheet);
+		Map<String, Integer> standardColumns = getStandardColumns(sheet);
+		Map<String, Integer> miscColumns = getMiscColumns(sheet);
 
 		KnimeTuple tuple = null;
 		String id = null;
 
 		for (int i = 1;; i++) {
-			boolean endOfFile = sheet.getRow(i) == null;
-
-			endOfFile = endOfFile || sheet.getRow(i).getCell(4) == null;
-			endOfFile = endOfFile || sheet.getRow(i).getCell(5) == null;
-			endOfFile = endOfFile
-					|| sheet.getRow(i).getCell(4).toString().trim().isEmpty();
-			endOfFile = endOfFile
-					|| sheet.getRow(i).getCell(5).toString().trim().isEmpty();
-
-			if (endOfFile) {
+			if (isEndOfFile(sheet, i)) {
 				if (tuple != null) {
 					tuples.put(id, tuple);
 				}
@@ -62,120 +98,128 @@ public class XLSReader {
 				break;
 			}
 
-			if (sheet.getRow(i).getCell(0) != null
-					&& !sheet.getRow(i).getCell(0).toString().trim().isEmpty()
-					&& !sheet.getRow(i).getCell(0).toString().trim().equals(id)) {
+			Row row = sheet.getRow(i);
+			Cell idCell = row.getCell(standardColumns.get(ID));
+			Cell agentCell = row.getCell(standardColumns
+					.get(TimeSeriesSchema.ATT_AGENTNAME));
+			Cell matrixCell = row.getCell(standardColumns
+					.get(TimeSeriesSchema.ATT_MATRIXNAME));
+			Cell commentCell = row.getCell(standardColumns
+					.get(TimeSeriesSchema.ATT_COMMENT));
+			Cell tempCell = row.getCell(standardColumns
+					.get(TimeSeriesSchema.ATT_TEMPERATURE));
+			Cell phCell = row.getCell(standardColumns
+					.get(TimeSeriesSchema.ATT_PH));
+			Cell awCell = row.getCell(standardColumns
+					.get(TimeSeriesSchema.ATT_WATERACTIVITY));
+			Cell timeCell = row.getCell(standardColumns
+					.get(TimeSeriesSchema.ATT_TIME));
+			Cell logcCell = row.getCell(standardColumns
+					.get(TimeSeriesSchema.ATT_LOGC));
+
+			if (idCell != null && !idCell.toString().trim().isEmpty()
+					&& !idCell.toString().trim().equals(id)) {
 				if (tuple != null) {
 					tuples.put(id, tuple);
 				}
 
-				id = sheet.getRow(i).getCell(0).toString().trim();
+				id = idCell.toString().trim();
 				tuple = new KnimeTuple(new TimeSeriesSchema());
 				tuple.setValue(TimeSeriesSchema.ATT_CONDID,
 						MathUtilities.getRandomNegativeInt());
 
-				if (sheet.getRow(i).getCell(1) != null) {
-					tuple.setValue(TimeSeriesSchema.ATT_AGENTDETAIL, sheet
-							.getRow(i).getCell(1).toString().trim());
+				if (agentCell != null) {
+					tuple.setValue(TimeSeriesSchema.ATT_AGENTDETAIL, agentCell
+							.toString().trim());
 				}
 
-				if (sheet.getRow(i).getCell(2) != null) {
-					tuple.setValue(TimeSeriesSchema.ATT_MATRIXDETAIL, sheet
-							.getRow(i).getCell(2).toString().trim());
+				if (matrixCell != null) {
+					tuple.setValue(TimeSeriesSchema.ATT_MATRIXDETAIL,
+							matrixCell.toString().trim());
 				}
 
-				if (sheet.getRow(i).getCell(3) != null) {
-					tuple.setValue(TimeSeriesSchema.ATT_COMMENT, sheet
-							.getRow(i).getCell(3).toString().trim());
+				if (commentCell != null) {
+					tuple.setValue(TimeSeriesSchema.ATT_COMMENT, commentCell
+							.toString().trim());
 				}
 
-				if (sheet.getRow(i).getCell(6) != null
-						&& !sheet.getRow(i).getCell(6).toString().trim()
-								.isEmpty()) {
+				if (tempCell != null && !tempCell.toString().trim().isEmpty()) {
 					try {
 						tuple.setValue(
 								TimeSeriesSchema.ATT_TEMPERATURE,
-								Double.parseDouble(sheet.getRow(i).getCell(6)
-										.toString().replace(",", ".")));
+								Double.parseDouble(tempCell.toString().replace(
+										",", ".")));
 					} catch (NumberFormatException e) {
-						throw new Exception("Temperature value in row "
-								+ (i + 1) + " is not valid");
+						throw new Exception(TimeSeriesSchema.ATT_TEMPERATURE
+								+ " value in row " + (i + 1) + " is not valid");
 					}
 				}
 
-				if (sheet.getRow(i).getCell(7) != null
-						&& !sheet.getRow(i).getCell(7).toString().trim()
-								.isEmpty()) {
+				if (phCell != null && !phCell.toString().trim().isEmpty()) {
 					try {
 						tuple.setValue(
 								TimeSeriesSchema.ATT_PH,
-								Double.parseDouble(sheet.getRow(i).getCell(7)
-										.toString().replace(",", ".")));
+								Double.parseDouble(phCell.toString().replace(
+										",", ".")));
 					} catch (NumberFormatException e) {
-						throw new Exception("pH value in row " + (i + 1)
-								+ " is not valid");
+						throw new Exception(TimeSeriesSchema.ATT_PH
+								+ " value in row " + (i + 1) + " is not valid");
 					}
 				}
 
-				if (sheet.getRow(i).getCell(8) != null
-						&& !sheet.getRow(i).getCell(8).toString().trim()
-								.isEmpty()) {
+				if (awCell != null && !awCell.toString().trim().isEmpty()) {
 					try {
 						tuple.setValue(
 								TimeSeriesSchema.ATT_WATERACTIVITY,
-								Double.parseDouble(sheet.getRow(i).getCell(8)
-										.toString().replace(",", ".")));
+								Double.parseDouble(awCell.toString().replace(
+										",", ".")));
 					} catch (NumberFormatException e) {
-						throw new Exception("Water Activity value in row "
-								+ (i + 1) + " is not valid");
+						throw new Exception(TimeSeriesSchema.ATT_WATERACTIVITY
+								+ " value in row " + (i + 1) + " is not valid");
 					}
 				}
 
 				PmmXmlDoc miscXML = new PmmXmlDoc();
 
-				for (int j = 0; j < miscNames.size(); j++) {
-					String name = miscNames.get(j);
+				for (String miscName : miscColumns.keySet()) {
+					Cell cell = row.getCell(miscColumns.get(miscName));
 					Double value = null;
 
 					try {
-						value = Double.parseDouble(sheet.getRow(i)
-								.getCell(j + 9).toString().replace(",", "."));
+						value = Double.parseDouble(cell.toString().replace(",",
+								"."));
 					} catch (Exception e) {
 					}
 
 					miscXML.add(new MiscXml(MathUtilities
-							.getRandomNegativeInt(), name, "", value, ""));
+							.getRandomNegativeInt(), miscName, "", value, ""));
 				}
 
 				tuple.setValue(TimeSeriesSchema.ATT_MISC, miscXML);
 			}
 
 			if (tuple != null) {
-				if (sheet.getRow(i).getCell(4) != null
-						&& !sheet.getRow(i).getCell(4).toString().trim()
-								.isEmpty()) {
+				if (timeCell != null && !timeCell.toString().trim().isEmpty()) {
 					try {
 						tuple.addValue(
 								TimeSeriesSchema.ATT_TIME,
-								Double.parseDouble(sheet.getRow(i).getCell(4)
-										.toString().replace(",", ".")));
+								Double.parseDouble(timeCell.toString().replace(
+										",", ".")));
 					} catch (NumberFormatException e) {
-						throw new Exception("Time value in row " + (i + 1)
-								+ " is not valid");
+						throw new Exception(TimeSeriesSchema.ATT_TIME
+								+ " value in row " + (i + 1) + " is not valid");
 					}
 				}
 
-				if (sheet.getRow(i).getCell(5) != null
-						&& !sheet.getRow(i).getCell(5).toString().trim()
-								.isEmpty()) {
+				if (logcCell != null && !logcCell.toString().trim().isEmpty()) {
 					try {
 						tuple.addValue(
 								TimeSeriesSchema.ATT_LOGC,
-								Double.parseDouble(sheet.getRow(i).getCell(5)
-										.toString().replace(",", ".")));
+								Double.parseDouble(logcCell.toString().replace(
+										",", ".")));
 					} catch (NumberFormatException e) {
-						throw new Exception("LogC value in row " + (i + 1)
-								+ " is not valid");
+						throw new Exception(TimeSeriesSchema.ATT_LOGC
+								+ " value in row " + (i + 1) + " is not valid");
 					}
 				}
 			}
@@ -184,20 +228,73 @@ public class XLSReader {
 		return tuples;
 	}
 
-	private static List<String> getAllMiscNames(Sheet sheet) {
-		List<String> miscNames = new ArrayList<String>();
+	private static Map<String, Integer> getStandardColumns(Sheet sheet)
+			throws Exception {
+		Map<String, Integer> standardColumns = new LinkedHashMap<String, Integer>();
 
-		for (int i = 9;; i++) {
+		for (int i = 0;; i++) {
 			Cell cell = sheet.getRow(0).getCell(i);
 
 			if (cell == null || cell.toString().trim().isEmpty()) {
 				break;
 			}
 
-			miscNames.add(cell.toString().trim());
+			String columnName = cell.toString().trim();
+
+			if (Arrays.asList(STANDARD_COLUMNS).contains(columnName)) {
+				standardColumns.put(columnName, i);
+			}
 		}
 
-		return miscNames;
+		for (String columnName : STANDARD_COLUMNS) {
+			if (!standardColumns.containsKey(columnName)) {
+				throw new Exception("Column " + columnName + " is missing");
+			}
+		}
+
+		return standardColumns;
 	}
 
+	private static Map<String, Integer> getMiscColumns(Sheet sheet) {
+		Map<String, Integer> miscColumns = new LinkedHashMap<String, Integer>();
+
+		for (int i = 0;; i++) {
+			Cell cell = sheet.getRow(0).getCell(i);
+
+			if (cell == null || cell.toString().trim().isEmpty()) {
+				break;
+			}
+
+			String columnName = cell.toString().trim();
+
+			if (!Arrays.asList(STANDARD_COLUMNS).contains(columnName)) {
+				miscColumns.put(columnName, i);
+			}
+		}
+
+		return miscColumns;
+	}
+
+	private static boolean isEndOfFile(Sheet sheet, int i) {
+		Row row = sheet.getRow(i);
+
+		if (row == null) {
+			return true;
+		}
+
+		for (int j = 0;; j++) {
+			Cell headerCell = sheet.getRow(0).getCell(j);
+			Cell cell = sheet.getRow(i).getCell(j);
+
+			if (headerCell == null || headerCell.toString().trim().isEmpty()) {
+				break;
+			}
+
+			if (cell != null && !cell.toString().trim().isEmpty()) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }

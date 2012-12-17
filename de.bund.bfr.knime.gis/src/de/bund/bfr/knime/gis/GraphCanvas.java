eba4d0d36c51c2212f29df463e8d0cbbffff8842
cthoens <christian.thoens@bfr.bund.de>
2012-12-17 16:19:01 +0100

    GIS nodes updated

--- de.bund.bfr.knime.gis/src/de/bund/bfr/knime/gis/GraphCanvas.java
+++ de.bund.bfr.knime.gis/src/de/bund/bfr/knime/gis/GraphCanvas.java
@@ -2,53 +2,133 @@ package de.bund.bfr.knime.gis;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Shape;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.apache.commons.collections15.Transformer;
 
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
-import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
 
-public class GraphCanvas extends JPanel {
+public class GraphCanvas extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
+	private static final String CIRCLE_LAYOUT = "Circle Layout";
+	private static final String FR_LAYOUT = "FR Layout";
+	private static final String[] LAYOUTS = { CIRCLE_LAYOUT, FR_LAYOUT };
+
+	private static final String DEFAULT_LAYOUT = CIRCLE_LAYOUT;
+	private static final int DEFAULT_NODESIZE = 10;
+
+	private Graph<Node, Edge> graph;
+	private VisualizationViewer<Node, Edge> viewer;
+
+	private JComboBox<String> layoutBox;
+	private JTextField nodeSizeField;
+	private JButton applyButton;
+
 	public GraphCanvas(List<Node> nodes, List<Edge> edges) {
-		Graph<Node, Edge> g = new SparseMultigraph<Node, Edge>();
+		graph = new SparseMultigraph<Node, Edge>();
 
 		for (Node node : nodes) {
-			g.addVertex(node);
+			graph.addVertex(node);
 		}
 
 		for (Edge edge : edges) {
-			g.addEdge(edge, edge.getN1(), edge.getN2());
+			graph.addEdge(edge, edge.getN1(), edge.getN2());
 		}
 
-		Layout<Node, Edge> layout = new CircleLayout<Node, Edge>(g);
+		viewer = createViewer(DEFAULT_LAYOUT, DEFAULT_NODESIZE);
+		setLayout(new BorderLayout());
+		add(viewer, BorderLayout.CENTER);
+		add(createOptionsPanel(), BorderLayout.SOUTH);
+	}
 
-		layout.setSize(new Dimension(1000, 1000));
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == applyButton) {
+			try {
+				remove(viewer);
+				add(createViewer((String) layoutBox.getSelectedItem(),
+						Integer.parseInt(nodeSizeField.getText())),
+						BorderLayout.CENTER);
+				revalidate();
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(this,
+						"Node Size must be Integer Value", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+
+	private VisualizationViewer<Node, Edge> createViewer(String layoutType,
+			int nodeSize) {
+		Dimension size = null;
+		Layout<Node, Edge> layout = null;
+
+		if (viewer != null) {
+			size = viewer.getSize();
+		} else {
+			size = new Dimension(400, 600);
+		}
+
+		if (layoutType.equals(CIRCLE_LAYOUT)) {
+			layout = new CircleLayout<Node, Edge>(graph);
+		} else if (layoutType.equals(FR_LAYOUT)) {
+			layout = new FRLayout<Node, Edge>(graph);
+			((FRLayout<Node, Edge>) layout).setMaxIterations(100);
+		}
+
+		layout.setSize(size);
 
 		VisualizationViewer<Node, Edge> vv = new VisualizationViewer<Node, Edge>(
 				layout);
 
-		vv.setPreferredSize(new Dimension(350, 350));
-		// vv.getRenderContext().setVertexLabelTransformer(
-		// new ToStringLabeller<Node>());
-		// vv.getRenderContext().setEdgeLabelTransformer(
-		// new ToStringLabeller<Edge>());
+		vv.setPreferredSize(size);
+		vv.setGraphMouse(new DefaultModalGraphMouse<Integer, String>());
+		vv.getRenderContext().setVertexShapeTransformer(
+				new ShapeTransformer(nodeSize));
 
-		DefaultModalGraphMouse<Integer, String> gm = new DefaultModalGraphMouse<Integer, String>();
+		return vv;
+	}
 
-		gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
-		vv.setGraphMouse(gm);
+	private JPanel createOptionsPanel() {
+		layoutBox = new JComboBox<String>(LAYOUTS);
+		layoutBox.setSelectedItem(DEFAULT_LAYOUT);
+		layoutBox.addActionListener(this);
+		nodeSizeField = new JTextField("" + DEFAULT_NODESIZE);
+		nodeSizeField.setPreferredSize(new Dimension(50, nodeSizeField
+				.getPreferredSize().height));
+		applyButton = new JButton("Apply");
+		applyButton.addActionListener(this);
 
-		setLayout(new BorderLayout());
-		add(vv, BorderLayout.CENTER);
+		JPanel panel = new JPanel();
+
+		panel.setLayout(new FlowLayout());
+		panel.add(new JLabel("Layout Type:"));
+		panel.add(layoutBox);
+		panel.add(new JLabel("Node Size:"));
+		panel.add(nodeSizeField);
+		panel.add(applyButton);
+
+		return panel;
 	}
 
 	public static class Node {
@@ -61,11 +141,6 @@ public class GraphCanvas extends JPanel {
 
 		public String getRegion() {
 			return region;
-		}
-
-		@Override
-		public String toString() {
-			return "Node [region=" + region + "]";
 		}
 	}
 
@@ -92,11 +167,24 @@ public class GraphCanvas extends JPanel {
 		public Node getN2() {
 			return n2;
 		}
+	}
+
+	private static class ShapeTransformer implements Transformer<Node, Shape> {
+
+		private int size;
+
+		public ShapeTransformer(int size) {
+			this.size = size;
+		}
 
 		@Override
-		public String toString() {
-			return "Edge [value=" + value + "]";
+		public Shape transform(Node i) {
+			Ellipse2D circle = new Ellipse2D.Double(-size / 2, -size / 2, size,
+					size);
+
+			return circle;
 		}
+
 	}
 
 }

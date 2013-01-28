b97876c00dc1fe745140192a1be97ca431d73f7d
draaw <armin.weiser@bfr.bund.de>
2013-01-28 23:10:47 +0100

    .

--- de.bund.bfr.knime.pmm.bfrdbiface.lib/src/org/hsh/bfr/db/DBKernel.java
+++ de.bund.bfr.knime.pmm.bfrdbiface.lib/src/org/hsh/bfr/db/DBKernel.java
@@ -134,12 +134,12 @@ public class DBKernel {
 	public static boolean createNewFirstDB = false && (DBKernel.debug || DBKernel.isKrise);
 	
 	public static String getTempSA(boolean other) {
-		if (debug) return "defad";
+		//if (debug) return "SA";
 		if (other) return isKNIME || isKrise ? "defad": "SA";		
 		else return isKNIME || isKrise ? "SA" : "defad";		
 	}
 	public static String getTempSAPass(boolean other) {
-		if (debug) return "de6!§5ddy";
+		//if (debug) return "";
 		if (isServerConnection && isKrise) return "de6!§5ddy";
 			
 		if (other) return isKNIME || isKrise ? "de6!§5ddy" : "";

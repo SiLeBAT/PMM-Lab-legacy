b0c4af8855faf5f7114e45771be4dbfa953f38cc
cthoens <christian.thoens@bfr.bund.de>
2013-01-29 12:59:05 +0100

    Simple XLS Reader committed (not a PmmLab node)

---
+++ de.bund.bfr.knime.simplexlsreader/src/de/bund/bfr/knime/simplexlsreader/SimpleXLSReaderNodeDialog.java
@@ -0,0 +1,36 @@
+package de.bund.bfr.knime.simplexlsreader;
+
+import javax.swing.JFileChooser;
+
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+
+/**
+ * <code>NodeDialog</code> for the "SimpleXLSReader" Node.
+ * 
+ * 
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more
+ * complex dialog please derive directly from
+ * {@link org.knime.core.node.NodeDialogPane}.
+ * 
+ * @author Christian Thöns
+ */
+public class SimpleXLSReaderNodeDialog extends DefaultNodeSettingsPane {
+
+	private static final String FILE_HISTORY = "FileHistory";
+
+	/**
+	 * New pane for configuring SimpleXLSReader node dialog. This is just a
+	 * suggestion to demonstrate possible default dialog components.
+	 */
+	protected SimpleXLSReaderNodeDialog() {
+		super();
+
+		addDialogComponent(new DialogComponentFileChooser(
+				new SettingsModelString(SimpleXLSReaderNodeModel.CFGKEY_FILENAME,
+						SimpleXLSReaderNodeModel.DEFAULT_FILENAME), FILE_HISTORY,
+				JFileChooser.OPEN_DIALOG, "xls"));
+	}
+}

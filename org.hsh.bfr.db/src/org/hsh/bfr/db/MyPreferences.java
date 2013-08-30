956e1602caf8bece199eb078a067a72de8eb5d84
draaw <armin.weiser@bfr.bund.de>
2013-08-30 11:29:33 +0200

    update

--- org.hsh.bfr.db/src/org/hsh/bfr/db/MyPreferences.java
+++ org.hsh.bfr.db/src/org/hsh/bfr/db/MyPreferences.java
@@ -1,3 +1,32 @@
+/*
+ * For Standalone-App compilation
+package org.hsh.bfr.db;
+
+import org.hsh.bfr.db.gui.Login;
+
+public class MyPreferences {
+
+	public java.util.prefs.Preferences prefsReg = java.util.prefs.Preferences.userNodeForPackage(Login.class);
+
+	public void prefsFlush() {
+	}
+
+	public void put(String key, String value) {
+		prefsReg.put(key, value);
+	}
+
+	public String get(String key, String defaultValue) {
+		return prefsReg.get(key, defaultValue);
+	}
+
+	public void putBoolean(String key, boolean value) {
+		prefsReg.putBoolean(key, value);
+	}
+
+	public boolean getBoolean(String key, boolean defaultValue) {
+		return prefsReg.getBoolean(key, defaultValue);
+	}
+}*/
 package org.hsh.bfr.db;
 
 import org.eclipse.core.runtime.preferences.InstanceScope;

97a98e74dc796d1f9afb492cd02c6f74138328f1
draaw <armin.weiser@bfr.bund.de>
2014-03-25 14:06:48 +0100

    tu

--- de.bund.bfr.knime.pmm.estimatedmodelreader/src/de/bund/bfr/knime/pmm/estimatedmodelreader/EmReaderUi.java
+++ de.bund.bfr.knime.pmm.estimatedmodelreader/src/de/bund/bfr/knime/pmm/estimatedmodelreader/EmReaderUi.java
@@ -11,7 +11,6 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.swing.*;
 import javax.swing.border.*;

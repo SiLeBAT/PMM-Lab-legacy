03caa4748b87b81eb5e8948d545eed4e34e0e6f5
draaw <armin.weiser@bfr.bund.de>
2014-10-22 17:07:13 +0200

    tu

--- de.bund.bfr.knime.pmm.common/src/de/bund/bfr/knime/pmm/common/DepXml.java
+++ de.bund.bfr.knime.pmm.common/src/de/bund/bfr/knime/pmm/common/DepXml.java
@@ -8,18 +8,22 @@ public class DepXml implements PmmXmlElementConvertable {
 
 	private static final String ATT_NAME = "name";
 	private static final String ATT_ORIGNAME = "origname";
+	private static final String ATT_MIN = "min";
+	private static final String ATT_MAX = "max";
 	private static final String ATT_CATEGORY = "category";
 	private static final String ATT_UNIT = "unit";
 	private static final String ATT_DESCRIPTION = "description";
 
 	private String name;
 	private String origName;
+	private Double min;
+	private Double max;
 	private String category;
 	private String unit;
 	private String description;
 
 	public DepXml(String name) {
-		this(name, null, null, null, null);
+		this(name, null, null);
 	}
 
 	public DepXml(String name, String category, String unit) {
@@ -40,6 +44,8 @@ public class DepXml implements PmmXmlElementConvertable {
 				ATT_ORIGNAME), XmlHelper.getString(el, ATT_CATEGORY), XmlHelper
 				.getString(el, ATT_UNIT), XmlHelper.getString(el,
 				ATT_DESCRIPTION));
+		this.setMin(XmlHelper.getDouble(el, ATT_MIN));
+		this.setMax(XmlHelper.getDouble(el, ATT_MAX));
 	}
 
 	@Override
@@ -48,6 +54,8 @@ public class DepXml implements PmmXmlElementConvertable {
 
 		ret.setAttribute(ATT_NAME, XmlHelper.getNonNull(name));
 		ret.setAttribute(ATT_ORIGNAME, XmlHelper.getNonNull(origName));
+		ret.setAttribute(ATT_MIN, XmlHelper.getNonNull(min));
+		ret.setAttribute(ATT_MAX, XmlHelper.getNonNull(max));
 		ret.setAttribute(ATT_CATEGORY, XmlHelper.getNonNull(category));
 		ret.setAttribute(ATT_UNIT, XmlHelper.getNonNull(unit));
 		ret.setAttribute(ATT_DESCRIPTION, XmlHelper.getNonNull(description));
@@ -69,6 +77,22 @@ public class DepXml implements PmmXmlElementConvertable {
 
 	public void setOrigName(String origName) {
 		this.origName = origName;
+	}
+
+	public Double getMin() {
+		return min;
+	}
+
+	public void setMin(Double min) {
+		this.min = min;
+	}
+
+	public Double getMax() {
+		return max;
+	}
+
+	public void setMax(Double max) {
+		this.max = max;
 	}
 
 	public String getCategory() {

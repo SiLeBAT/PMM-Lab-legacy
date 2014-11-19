bd31817c10ff1a0b9356502dc9874ab53f571544
thoens <christian.thoens@gmail.com>
2014-11-19 17:25:15 +0100

    Revert "PredictorView now ignores parameter units"
    This reverts commit 4009e143c79a9e1fc8d471e68cf5bb161866d032.

--- de.bund.bfr.knime.pmm.predictorview/src/de/bund/bfr/knime/pmm/predictorview/TableReader.java
+++ de.bund.bfr.knime.pmm.predictorview/src/de/bund/bfr/knime/pmm/predictorview/TableReader.java
@@ -384,9 +384,9 @@ public class TableReader {
 				if (element.getName().equals(initParam)
 						|| element.getName().equals(lagParam)) {
 					variables.put(element.getName(), new ArrayList<Double>());
-					// units.put(element.getName(), element.getUnit());
-					// categories.put(element.getName(),
-					// Arrays.asList(element.getCategory()));
+					units.put(element.getName(), element.getUnit());
+					categories.put(element.getName(),
+							Arrays.asList(element.getCategory()));
 
 					if (element.getName().equals(initParam)
 							&& minConcentration != null

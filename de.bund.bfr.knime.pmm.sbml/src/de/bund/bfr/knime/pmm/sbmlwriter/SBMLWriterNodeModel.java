1b9a5ae8afa3a1e54070715b92fb10d0a924f229
thoens <christian.thoens@gmail.com>
2015-10-02 13:08:41 +0200

    old SBMLWriter added

---
+++ de.bund.bfr.knime.pmm.sbml/src/de/bund/bfr/knime/pmm/sbmlwriter/SBMLWriterNodeModel.java
@@ -0,0 +1,241 @@
+/*******************************************************************************
+ * PMM-Lab © 2012, Federal Institute for Risk Assessment (BfR), Germany
+ * 
+ * PMM-Lab is a set of KNIME-Nodes and KNIME workflows running within the KNIME software plattform (http://www.knime.org.).
+ * 
+ * PMM-Lab © 2012, Federal Institute for Risk Assessment (BfR), Germany
+ * Contact: armin.weiser@bfr.bund.de or matthias.filter@bfr.bund.de 
+ * 
+ * Developers and contributors to the PMM-Lab project are 
+ * Joergen Brandt (BfR)
+ * Armin A. Weiser (BfR)
+ * Matthias Filter (BfR)
+ * Alexander Falenski (BfR)
+ * Christian Thoens (BfR)
+ * Annemarie Kaesbohrer (BfR)
+ * Bernd Appel (BfR)
+ * 
+ * PMM-Lab is a project under development. Contributions are welcome.
+ * 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package de.bund.bfr.knime.pmm.sbmlwriter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelDate;
+import org.knime.core.node.defaultnodesettings.SettingsModelOptionalString;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.sbml.jsbml.SBMLWriter;
+
+import de.bund.bfr.knime.pmm.common.generictablemodel.KnimeSchema;
+import de.bund.bfr.knime.pmm.common.pmmtablemodel.PmmUtilities;
+import de.bund.bfr.knime.pmm.common.pmmtablemodel.SchemaFactory;
+
+/**
+ * This is the model implementation of SBMLWriter.
+ * 
+ * 
+ * @author Christian Thoens
+ */
+public class SBMLWriterNodeModel extends NodeModel {
+
+	protected static final String CFG_OVERWRITE = "Overwrite";
+	protected static final String CFG_OUT_PATH = "outPath";
+	protected static final String CFG_VARIABLE_PARAM = "variableParams";
+	protected static final String CFG_CREATOR_GIVEN_NAME = "CreatorGivenName";
+	protected static final String CFG_CREATOR_FAMILY_NAME = "CreatorFamilyName";
+	protected static final String CFG_CREATOR_CONTACT = "CreatorContact";
+	protected static final String CFG_CREATED_DATE = "CreationDate";
+	protected static final String CFG_LAST_MODIFIED_DATE = "ModifiedDate";
+	protected static final String CFG_REFERENCE = "Reference";
+
+	private SettingsModelBoolean overwrite = new SettingsModelBoolean(
+			CFG_OVERWRITE, false);
+	private SettingsModelString outPath = new SettingsModelString(CFG_OUT_PATH,
+			null);
+	private SettingsModelString variableParams = new SettingsModelOptionalString(
+			CFG_VARIABLE_PARAM, null, false);
+	private SettingsModelString creatorGivenName = new SettingsModelString(
+			CFG_CREATOR_GIVEN_NAME, null);
+	private SettingsModelString creatorFamilyName = new SettingsModelString(
+			CFG_CREATOR_FAMILY_NAME, null);
+	private SettingsModelString creatorContact = new SettingsModelString(
+			CFG_CREATOR_CONTACT, null);
+	private SettingsModelDate createdDate = new SettingsModelDate(
+			CFG_CREATED_DATE);
+	private SettingsModelDate modifiedDate = new SettingsModelDate(
+			CFG_LAST_MODIFIED_DATE);
+	private SettingsModelString reference = new SettingsModelString(
+			CFG_REFERENCE, null);
+
+	private KnimeSchema schema;
+
+	/**
+	 * Constructor for the node model.
+	 */
+	protected SBMLWriterNodeModel() {
+		super(1, 0);
+		schema = null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
+			final ExecutionContext exec) throws Exception {
+		TableReader reader = new TableReader(PmmUtilities.getTuples(inData[0],
+				schema), variableParams.getStringValue(),
+				creatorGivenName.getStringValue(),
+				creatorFamilyName.getStringValue(),
+				creatorContact.getStringValue(), getDate(createdDate),
+				getDate(modifiedDate), reference.getStringValue());
+		Map<String, File> files = new LinkedHashMap<>();
+
+		for (String name : reader.getDocuments().keySet()) {
+			File file = new File(outPath.getStringValue() + "/" + name
+					+ ".sbml.xml");
+
+			if (!overwrite.getBooleanValue() && file.exists()) {
+				throw new IOException(file.getAbsolutePath()
+						+ " already exists");
+			}
+
+			files.put(name, file);
+		}
+
+		for (String name : reader.getDocuments().keySet()) {
+			SBMLWriter.write(reader.getDocuments().get(name), files.get(name),
+					name, "1.0");
+		}
+
+		return new BufferedDataTable[] {};
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void reset() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
+			throws InvalidSettingsException {
+		if (SchemaFactory.createM12DataSchema().conforms(inSpecs[0])) {
+			schema = SchemaFactory.createM12DataSchema();
+		} else if (SchemaFactory.createM1DataSchema().conforms(inSpecs[0])) {
+			schema = SchemaFactory.createM1DataSchema();
+		} else if (outPath.getStringValue() == null
+				|| variableParams.getStringValue() == null) {
+			throw new InvalidSettingsException("Node must be configured");
+		} else {
+			throw new InvalidSettingsException("Invalid Input");
+		}
+
+		return new DataTableSpec[] {};
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void saveSettingsTo(final NodeSettingsWO settings) {
+		overwrite.saveSettingsTo(settings);
+		outPath.saveSettingsTo(settings);
+		variableParams.saveSettingsTo(settings);
+		creatorGivenName.saveSettingsTo(settings);
+		creatorFamilyName.saveSettingsTo(settings);
+		creatorContact.saveSettingsTo(settings);
+		createdDate.saveSettingsTo(settings);
+		modifiedDate.saveSettingsTo(settings);
+		reference.saveSettingsTo(settings);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+			throws InvalidSettingsException {
+		overwrite.loadSettingsFrom(settings);
+		outPath.loadSettingsFrom(settings);
+		variableParams.loadSettingsFrom(settings);
+		creatorGivenName.loadSettingsFrom(settings);
+		creatorFamilyName.loadSettingsFrom(settings);
+		creatorContact.loadSettingsFrom(settings);
+		createdDate.loadSettingsFrom(settings);
+		modifiedDate.loadSettingsFrom(settings);
+		reference.loadSettingsFrom(settings);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void validateSettings(final NodeSettingsRO settings)
+			throws InvalidSettingsException {
+		overwrite.validateSettings(settings);
+		outPath.validateSettings(settings);
+		variableParams.validateSettings(settings);
+		creatorGivenName.validateSettings(settings);
+		creatorFamilyName.validateSettings(settings);
+		creatorContact.validateSettings(settings);
+		createdDate.validateSettings(settings);
+		modifiedDate.validateSettings(settings);
+		reference.validateSettings(settings);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void loadInternals(final File internDir,
+			final ExecutionMonitor exec) throws IOException,
+			CanceledExecutionException {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void saveInternals(final File internDir,
+			final ExecutionMonitor exec) throws IOException,
+			CanceledExecutionException {
+	}
+
+	private static Date getDate(SettingsModelDate date) {
+		return date.getSelectedFields() != 0 ? date.getDate() : null;
+	}
+
+}

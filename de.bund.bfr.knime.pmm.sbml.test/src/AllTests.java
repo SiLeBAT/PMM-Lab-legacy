69e8eea8ec643336ae2b2e1f7f39c1ed0a49a06a
miguelalba89 <miguelalba89@gmail.com>
2015-12-08 16:37:28 +0100

    Removed JS Editor from plugin list and restored test suites

--- de.bund.bfr.knime.pmm.sbml.test/src/AllTests.java
+++ de.bund.bfr.knime.pmm.sbml.test/src/AllTests.java
@@ -17,3 +17,17 @@
  * Contributors:
  *     Department Biological Safety - BfR
  *******************************************************************************/
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import de.bund.bfr.knime.pmm.openfsmr.FSMRTemplateImplTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	// de.bund.bfr.knime.pmm.openfsmr
+	FSMRTemplateImplTest.class
+	})
+public class AllTests {
+
+}

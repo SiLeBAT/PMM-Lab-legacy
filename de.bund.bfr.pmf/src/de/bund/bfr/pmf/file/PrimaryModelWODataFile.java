584a7345031109a5124feb2dfa40602f5afd3811
miguelalba89 <miguelalba89@gmail.com>
2016-02-08 07:14:55 +0100

    PMF library: PrimaryModelWODataFile
    PrimaryModelWODataFile handles exceptions

--- de.bund.bfr.pmf/src/de/bund/bfr/pmf/file/PrimaryModelWODataFile.java
+++ de.bund.bfr.pmf/src/de/bund/bfr/pmf/file/PrimaryModelWODataFile.java
@@ -1,43 +1,49 @@
 /*******************************************************************************
  * Copyright (c) 2015 Federal Institute for Risk Assessment (BfR), Germany
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
  *
- * Contributors:
- *     Department Biological Safety - BfR
+ * Contributors: Department Biological Safety - BfR
  *******************************************************************************/
 package de.bund.bfr.pmf.file;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
+import java.text.ParseException;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.TransformerException;
+
 import org.jdom2.Element;
+import org.jdom2.JDOMException;
 import org.sbml.jsbml.SBMLDocument;
+import org.sbml.jsbml.SBMLException;
 import org.sbml.jsbml.SBMLReader;
 import org.sbml.jsbml.SBMLWriter;
 
 import de.bund.bfr.pmf.ModelType;
+import de.bund.bfr.pmf.file.uri.PMFURI;
 import de.bund.bfr.pmf.file.uri.URIFactory;
 import de.bund.bfr.pmf.model.PrimaryModelWOData;
 import de.unirostock.sems.cbarchive.ArchiveEntry;
 import de.unirostock.sems.cbarchive.CombineArchive;
+import de.unirostock.sems.cbarchive.CombineArchiveException;
 import de.unirostock.sems.cbarchive.meta.DefaultMetaDataObject;
 
 /**
@@ -46,81 +52,132 @@ import de.unirostock.sems.cbarchive.meta.DefaultMetaDataObject;
  * @author Miguel Alba
  */
 public class PrimaryModelWODataFile {
-	
-	private static final URI SBML_URI = URIFactory.createNuMLURI();
-	private static final URI PMF_URI = URIFactory.createPMFURI();
-	
-	private static final SBMLReader READER = new SBMLReader();
-	private static final SBMLWriter WRITER = new SBMLWriter();
-	
-	public static List<PrimaryModelWOData> readPMF(String filename) throws Exception {
-		return read(filename, SBML_URI);
-	}
-	
-	public static List<PrimaryModelWOData> readPMFX(String filename) throws Exception {
-		return read(filename, PMF_URI);
-	}
-	
-	public static void writePMF(String dir, String filename, List<PrimaryModelWOData> models) throws Exception {
-		String caName = String.format("%s/%s.pmf", dir, filename);
-		write(caName, SBML_URI, models);
-	}
 
-	public static void writePMFX(String dir, String filename, List<PrimaryModelWOData> models) throws Exception {
-		String caName = String.format("%s/%s.pmfx", dir, filename);
-		write(caName, PMF_URI, models);
-	}
+  private static final URI SBML_URI = URIFactory.createNuMLURI();
+  private static final URI PMF_URI = URIFactory.createPMFURI();
 
-	private static List<PrimaryModelWOData> read(String filename, URI modelURI) throws Exception {
-		
-		List<PrimaryModelWOData> models = new LinkedList<>();
-		
-		// Creates CombineArchive
-		CombineArchive ca = new CombineArchive(new File(filename));
-		
-		// Parse models in the PMF file
-		List<ArchiveEntry> modelEntries = ca.getEntriesWithFormat(modelURI);
-		for (ArchiveEntry modelEntry : modelEntries) {
-			InputStream stream = Files.newInputStream(modelEntry.getPath(), StandardOpenOption.READ);
-			SBMLDocument sbmlDoc = READER.readSBMLFromStream(stream);
-			stream.close();
-			String sbmlDocName = modelEntry.getFileName();
-			PrimaryModelWOData model = new PrimaryModelWOData(sbmlDocName, sbmlDoc);
-			models.add(model);
-		}
-		
-		ca.close();
-		return models;
-	}
+  private static final SBMLReader READER = new SBMLReader();
+  private static final SBMLWriter WRITER = new SBMLWriter();
 
-	private static void write(String filename, URI modelURI, List<PrimaryModelWOData> models) throws Exception {
-		
-		// Removes previous CombineArchive if it exists
-		File fileTmp = new File(filename);
-		if (fileTmp.exists()) {
-			fileTmp.delete();
-		}
-		
-		// Creates new CombineArchive
-		CombineArchive ca = new CombineArchive(new File(filename));
-		
-		// Adds models and data
-		for (PrimaryModelWOData model : models) {
-			// Creates tmp file for the model
-			File sbmlTmp = File.createTempFile("temp1", "");
-			sbmlTmp.deleteOnExit();
-			
-			// Writes model to sbmlTmp and adds it to the file
-			WRITER.write(model.getDoc(), sbmlTmp);
-			ca.addEntry(sbmlTmp,  model.getDocName(), modelURI);
-		}
-		
-		// Adds description with model type
-		ModelType modelType = ModelType.PRIMARY_MODEL_WODATA;
-		Element metadataAnnotation = new PMFMetadataNode(modelType, new HashSet<String>(0)).node;
-		ca.addDescription(new DefaultMetaDataObject(metadataAnnotation));
-		
-		ca.pack();
-		ca.close();
-	}
+  public static List<PrimaryModelWOData> readPMF(String filename) throws CombineArchiveException {
+    return read(filename, SBML_URI);
+  }
+
+  public static List<PrimaryModelWOData> readPMFX(String filename) throws CombineArchiveException {
+    return read(filename, PMF_URI);
+  }
+
+  public static void writePMF(String dir, String filename, List<PrimaryModelWOData> models)
+      throws Exception {
+    String caName = String.format("%s/%s.pmf", dir, filename);
+    write(caName, SBML_URI, models);
+  }
+
+  public static void writePMFX(String dir, String filename, List<PrimaryModelWOData> models)
+      throws Exception {
+    String caName = String.format("%s/%s.pmfx", dir, filename);
+    write(caName, PMF_URI, models);
+  }
+
+
+  /**
+   * Reads primary models without data from a PMF or PMFX file. Faulty files are skipped.
+   *
+   * @param filename
+   * @param modelURI URI used for the models: {@link PMFURI} or {@link SBMLUri}
+   * @throws CombineArchivException if the CombineArchive could not be opened or closed properly
+   */
+  private static List<PrimaryModelWOData> read(final String filename, final URI modelURI)
+      throws CombineArchiveException {
+
+    final CombineArchive combineArchive;
+    try {
+      combineArchive = new CombineArchive(new File(filename));
+    } catch (IOException | JDOMException | ParseException | CombineArchiveException e) {
+      throw new CombineArchiveException(filename + " could not be opened");
+    }
+
+    final List<PrimaryModelWOData> models = new LinkedList<>();
+
+    // Parse models in the combineArchive
+    final List<ArchiveEntry> modelEntries = combineArchive.getEntriesWithFormat(modelURI);
+    for (final ArchiveEntry entry : modelEntries) {
+      final String docName = entry.getFileName();
+
+      try {
+        final InputStream stream = Files.newInputStream(entry.getPath(), StandardOpenOption.READ);
+        final SBMLDocument doc = READER.readSBMLFromStream(stream);
+        stream.close();
+
+        models.add(new PrimaryModelWOData(docName, doc));
+      } catch (IOException | XMLStreamException e) {
+        System.err.println(docName + " could not be retrieved");
+        e.printStackTrace();
+      }
+    }
+
+    try {
+      combineArchive.close();
+    } catch (IOException e) {
+      throw new CombineArchiveException(filename + " could not be closed");
+    }
+
+    return models;
+  }
+
+  /**
+   * Writes primary models without data to a PMF or PMFX file. Faulty data files are skipped.
+   * Existent files with the same filename are overwritten.
+   *
+   * @param filename
+   * @parma models
+   * @throws CombineArchiveException if the CombineArchive cannot be opened or closed properly
+   */
+  private static void write(final String filename, final URI modelURI,
+      final List<PrimaryModelWOData> models) throws CombineArchiveException {
+
+    // Removes previous CombineArchive if it exists
+    final File tmpFile = new File(filename);
+    if (tmpFile.exists()) {
+      tmpFile.delete();
+    }
+
+
+    // Creates new CombineArchive
+    final CombineArchive combineArchive;
+    try {
+      combineArchive = new CombineArchive(new File(filename));
+    } catch (IOException | JDOMException | ParseException | CombineArchiveException e) {
+      throw new CombineArchiveException(filename + " could not be opened");
+    }
+
+    // Add models
+    for (final PrimaryModelWOData model : models) {
+      try {
+        // Creates temporary file for the model
+        File sbmlFile = File.createTempFile("temp", "");
+        sbmlFile.deleteOnExit();
+
+        // Writes model to tmpFile and adds it to the file
+        WRITER.write(model.getDoc(), sbmlFile);
+        combineArchive.addEntry(sbmlFile, model.getDocName(), modelURI);
+      } catch (SBMLException | XMLStreamException | IOException e) {
+        System.err.println(model.getDocName() + " could not be saved");
+        e.printStackTrace();
+      }
+    }
+
+    // Adds description with model type
+    final ModelType modelType = ModelType.PRIMARY_MODEL_WODATA;
+    final Element metadataAnnotation = new PMFMetadataNode(modelType, new HashSet<String>(0)).node;
+    combineArchive.addDescription(new DefaultMetaDataObject(metadataAnnotation));
+
+    // Packs and closes the combineArchive
+    try {
+      combineArchive.pack();
+      combineArchive.close();
+    } catch (IOException | TransformerException e) {
+      throw new CombineArchiveException(filename + " could not be created");
+    }
+  }
 }

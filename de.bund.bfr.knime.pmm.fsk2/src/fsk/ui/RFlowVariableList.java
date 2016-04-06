e22add8acb9a31993b14e2239c7bfd27d2b493b0
miguelalba89 <miguelalba89@gmail.com>
2016-04-06 11:48:07 +0200

    Add testing fsk project with a R2 compatible R Snippet Node

---
+++ de.bund.bfr.knime.pmm.fsk2/src/fsk/ui/RFlowVariableList.java
@@ -0,0 +1,131 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
+ *
+ * History
+ *   24.11.2011 (hofer): created
+ */
+package fsk.ui;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Collection;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+
+import org.knime.core.node.util.FlowVariableListCellRenderer;
+import org.knime.core.node.workflow.FlowVariable;
+
+/**
+ * A component that presents a list of flow variables for the snippet dialog.
+ * 
+ * @author Heiko Hofer.
+ */
+public class RFlowVariableList extends JList<FlowVariable> {
+
+  /** Generated serialVersionUID. */
+  private static final long serialVersionUID = 5577767593318832287L;
+
+  private RSnippetTextArea m_snippet;
+
+  public RFlowVariableList() {
+    super(new DefaultListModel<>());
+
+    setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    setToolTipText(""); // enable tooltip
+
+    addKeyListener(new KeyAdapter() {
+      /** {@inheritDoc} */
+      @Override
+      public void keyTyped(final KeyEvent e) {
+        if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+          Object selected = getSelectedValue();
+          if (selected != null) {
+            onSelectionInVariableList(selected);
+          }
+        }
+      }
+    });
+
+    setCellRenderer(new FlowVariableListCellRenderer());
+  }
+
+  private void onSelectionInVariableList(final Object selected) {
+    if (selected != null && selected instanceof FlowVariable) {
+      FlowVariable v = (FlowVariable) selected;
+      String enter = getFieldReadStatement(v);
+      clearSelection();
+      if (m_snippet != null) {
+        m_snippet.replaceSelection(enter);
+        m_snippet.requestFocus();
+      }
+    }
+  }
+
+  private String getFieldReadStatement(final FlowVariable v) {
+    return "knime.flow.in[[\"" + v.getName() + "\"]]";
+  }
+
+  /**
+   * A double click on an element will perform an insertion to this text area.
+   * 
+   * @param snippet the text area.
+   */
+  public void install(final RSnippetTextArea snippet) {
+    m_snippet = snippet;
+  }
+
+
+  /**
+   * Set the flow variables to display.
+   * 
+   * @param flowVars the flow variables.
+   */
+  public void setFlowVariables(final Collection<FlowVariable> flowVars) {
+    DefaultListModel<FlowVariable> fvListModel = (DefaultListModel<FlowVariable>) getModel();
+    fvListModel.removeAllElements();
+    flowVars.forEach(fvListModel::addElement);
+  }
+}

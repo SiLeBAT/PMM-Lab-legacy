5dd02e7e976849ce9cd991277778a68c3b722c78
miguelalba <Miguel.de-Alba-Aparicio@bfr.bund.de>
2016-07-15 17:46:56 +0200

    CC: Simplified dlg in SBML Writer

--- de.bund.bfr.knime.pmm.sbml/src/de/bund/bfr/knime/pmm/sbmlwriter/SBMLWriterNodeFactory.java
+++ de.bund.bfr.knime.pmm.sbml/src/de/bund/bfr/knime/pmm/sbmlwriter/SBMLWriterNodeFactory.java
@@ -16,9 +16,22 @@
  *******************************************************************************/
 package de.bund.bfr.knime.pmm.sbmlwriter;
 
+import javax.swing.JFileChooser;
+
 import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeFactory;
 import org.knime.core.node.NodeView;
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
+import org.knime.core.node.defaultnodesettings.DialogComponentDate;
+import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
+import org.knime.core.node.defaultnodesettings.DialogComponentMultiLineString;
+import org.knime.core.node.defaultnodesettings.DialogComponentOptionalString;
+import org.knime.core.node.defaultnodesettings.DialogComponentString;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelDate;
+import org.knime.core.node.defaultnodesettings.SettingsModelOptionalString;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
 /**
  * <code>NodeFactory</code> for the "SBMLWriter" Node.
@@ -66,7 +79,56 @@ public class SBMLWriterNodeFactory extends NodeFactory<SBMLWriterNodeModel> {
    */
   @Override
   public NodeDialogPane createNodeDialogPane() {
-    return new SBMLWriterNodeDialog();
+    // Create components
+    DialogComponentFileChooser outComp =
+        new DialogComponentFileChooser(new SettingsModelString(SBMLWriterNodeModel.CFG_OUT_PATH,
+            null), "Out History", JFileChooser.SAVE_DIALOG, true);
+    outComp.setBorderTitle("Output Path");
+    
+    DialogComponentBoolean overwriteComp =
+        new DialogComponentBoolean(new SettingsModelBoolean(SBMLWriterNodeModel.CFG_OVERWRITE,
+            false), "Overwrite OK");
+    
+    DialogComponentOptionalString varParamComp =
+        new DialogComponentOptionalString(new SettingsModelOptionalString(
+            SBMLWriterNodeModel.CFG_VARIABLE_PARAM, null, false), "Initial Concentration Parameter");
+    
+    DialogComponentString givenNameComp =
+        new DialogComponentString(new SettingsModelString(
+            SBMLWriterNodeModel.CFG_CREATOR_GIVEN_NAME, null), "Creator Given Name");
+    
+    DialogComponentString familyNameComp =
+        new DialogComponentString(new SettingsModelString(
+            SBMLWriterNodeModel.CFG_CREATOR_FAMILY_NAME, null), "Creator Family Name");
+    
+    DialogComponentString creatorContactComp =
+        new DialogComponentString(new SettingsModelString(SBMLWriterNodeModel.CFG_CREATOR_CONTACT,
+            null), "Creator Contact");
+    
+    DialogComponentDate createdComp =
+        new DialogComponentDate(new SettingsModelDate(SBMLWriterNodeModel.CFG_CREATED_DATE),
+            "Created");
+    
+    DialogComponentDate modifiedComp =
+        new DialogComponentDate(new SettingsModelDate(SBMLWriterNodeModel.CFG_LAST_MODIFIED_DATE),
+            "Last Modified");
+    
+    DialogComponentMultiLineString refComp =
+        new DialogComponentMultiLineString(new SettingsModelString(
+            SBMLWriterNodeModel.CFG_REFERENCE, null), "Reference as XHTML");
+
+    DefaultNodeSettingsPane pane = new DefaultNodeSettingsPane();
+    pane.addDialogComponent(outComp);
+    pane.addDialogComponent(overwriteComp);
+    pane.addDialogComponent(varParamComp);
+    pane.addDialogComponent(givenNameComp);
+    pane.addDialogComponent(familyNameComp);
+    pane.addDialogComponent(creatorContactComp);
+    pane.addDialogComponent(createdComp);
+    pane.addDialogComponent(modifiedComp);
+    pane.addDialogComponent(refComp);
+    
+    return pane;
   }
 
 }

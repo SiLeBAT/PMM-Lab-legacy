880b75b6f89507d34dcf77d6c549297faa971d9a
miguelalba <miguel.de-alba-aparicio@bfr.bund.de>
2017-12-03 14:29:44 +0100

    Improved layout of the SBML Writer node

--- de.bund.bfr.knime.pmm.nodes/src/de/bund/bfr/knime/pmm/sbmlwriter/SBMLWriterNodeModel.java
+++ de.bund.bfr.knime.pmm.nodes/src/de/bund/bfr/knime/pmm/sbmlwriter/SBMLWriterNodeModel.java
@@ -34,10 +34,6 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
-import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
-import org.knime.core.node.defaultnodesettings.SettingsModelDate;
-import org.knime.core.node.defaultnodesettings.SettingsModelOptionalString;
-import org.knime.core.node.defaultnodesettings.SettingsModelString;
 import org.sbml.jsbml.SBMLWriter;
 
 import de.bund.bfr.knime.pmm.common.generictablemodel.KnimeSchema;
@@ -46,40 +42,13 @@ import de.bund.bfr.knime.pmm.common.pmmtablemodel.SchemaFactory;
 
 /**
  * This is the model implementation of SBMLWriter.
- * 
- * 
+ *
  * @author Christian Thoens
+ * @author Miguel de Alba
  */
 public class SBMLWriterNodeModel extends NodeModel {
 
-	protected static final String CFG_OVERWRITE = "Overwrite";
-	protected static final String CFG_OUT_PATH = "outPath";
-	protected static final String CFG_VARIABLE_PARAM = "variableParams";
-	protected static final String CFG_CREATOR_GIVEN_NAME = "CreatorGivenName";
-	protected static final String CFG_CREATOR_FAMILY_NAME = "CreatorFamilyName";
-	protected static final String CFG_CREATOR_CONTACT = "CreatorContact";
-	protected static final String CFG_CREATED_DATE = "CreationDate";
-	protected static final String CFG_LAST_MODIFIED_DATE = "ModifiedDate";
-	protected static final String CFG_REFERENCE = "Reference";
-
-	private SettingsModelBoolean overwrite = new SettingsModelBoolean(
-			CFG_OVERWRITE, false);
-	private SettingsModelString outPath = new SettingsModelString(CFG_OUT_PATH,
-			null);
-	private SettingsModelString variableParams = new SettingsModelOptionalString(
-			CFG_VARIABLE_PARAM, null, false);
-	private SettingsModelString creatorGivenName = new SettingsModelString(
-			CFG_CREATOR_GIVEN_NAME, null);
-	private SettingsModelString creatorFamilyName = new SettingsModelString(
-			CFG_CREATOR_FAMILY_NAME, null);
-	private SettingsModelString creatorContact = new SettingsModelString(
-			CFG_CREATOR_CONTACT, null);
-	private SettingsModelDate createdDate = new SettingsModelDate(
-			CFG_CREATED_DATE);
-	private SettingsModelDate modifiedDate = new SettingsModelDate(
-			CFG_LAST_MODIFIED_DATE);
-	private SettingsModelString reference = new SettingsModelString(
-			CFG_REFERENCE, null);
+	private final SBMLWriterNodeSettings nodeSettings = new SBMLWriterNodeSettings();
 
 	private KnimeSchema schema;
 
@@ -91,59 +60,42 @@ public class SBMLWriterNodeModel extends NodeModel {
 		schema = null;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
-			final ExecutionContext exec) throws Exception {
-		TableReader reader = new TableReader(PmmUtilities.getTuples(inData[0],
-				schema), variableParams.getStringValue(),
-				creatorGivenName.getStringValue(),
-				creatorFamilyName.getStringValue(),
-				creatorContact.getStringValue(), getDate(createdDate),
-				getDate(modifiedDate), reference.getStringValue());
+	protected BufferedDataTable[] execute(final BufferedDataTable[] inData, final ExecutionContext exec)
+			throws Exception {
+		TableReader reader = new TableReader(PmmUtilities.getTuples(inData[0], schema), nodeSettings.variableParams,
+				nodeSettings.creatorGivenName, nodeSettings.creatorFamilyName, nodeSettings.creatorContact,
+				new Date(nodeSettings.createdDate), new Date(nodeSettings.modifiedDate), nodeSettings.reference);
 		Map<String, File> files = new LinkedHashMap<>();
 
 		for (String name : reader.getDocuments().keySet()) {
-			File file = new File(outPath.getStringValue() + "/" + name
-					+ ".sbml.xml");
+			File file = new File(nodeSettings.outPath + "/" + name + ".sbml.xml");
 
-			if (!overwrite.getBooleanValue() && file.exists()) {
-				throw new IOException(file.getAbsolutePath()
-						+ " already exists");
+			if (!nodeSettings.overwrite && file.exists()) {
+				throw new IOException(file.getAbsolutePath() + " already exists");
 			}
 
 			files.put(name, file);
 		}
 
 		for (String name : reader.getDocuments().keySet()) {
-			SBMLWriter.write(reader.getDocuments().get(name), files.get(name),
-					name, "1.0");
+			SBMLWriter.write(reader.getDocuments().get(name), files.get(name), name, "1.0");
 		}
 
 		return new BufferedDataTable[] {};
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	protected void reset() {
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
-			throws InvalidSettingsException {
+	protected DataTableSpec[] configure(final DataTableSpec[] inSpecs) throws InvalidSettingsException {
 		if (SchemaFactory.createM12DataSchema().conforms(inSpecs[0])) {
 			schema = SchemaFactory.createM12DataSchema();
 		} else if (SchemaFactory.createM1DataSchema().conforms(inSpecs[0])) {
 			schema = SchemaFactory.createM1DataSchema();
-		} else if (outPath.getStringValue() == null
-				|| variableParams.getStringValue() == null) {
+		} else if (nodeSettings.outPath == null || nodeSettings.variableParams == null) {
 			throw new InvalidSettingsException("Node must be configured");
 		} else {
 			throw new InvalidSettingsException("Invalid Input");
@@ -152,76 +104,28 @@ public class SBMLWriterNodeModel extends NodeModel {
 		return new DataTableSpec[] {};
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	protected void saveSettingsTo(final NodeSettingsWO settings) {
-		overwrite.saveSettingsTo(settings);
-		outPath.saveSettingsTo(settings);
-		variableParams.saveSettingsTo(settings);
-		creatorGivenName.saveSettingsTo(settings);
-		creatorFamilyName.saveSettingsTo(settings);
-		creatorContact.saveSettingsTo(settings);
-		createdDate.saveSettingsTo(settings);
-		modifiedDate.saveSettingsTo(settings);
-		reference.saveSettingsTo(settings);
+		nodeSettings.save(settings);
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
-			throws InvalidSettingsException {
-		overwrite.loadSettingsFrom(settings);
-		outPath.loadSettingsFrom(settings);
-		variableParams.loadSettingsFrom(settings);
-		creatorGivenName.loadSettingsFrom(settings);
-		creatorFamilyName.loadSettingsFrom(settings);
-		creatorContact.loadSettingsFrom(settings);
-		createdDate.loadSettingsFrom(settings);
-		modifiedDate.loadSettingsFrom(settings);
-		reference.loadSettingsFrom(settings);
+	protected void loadValidatedSettingsFrom(final NodeSettingsRO settings) throws InvalidSettingsException {
+		nodeSettings.load(settings);
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	protected void validateSettings(final NodeSettingsRO settings)
-			throws InvalidSettingsException {
-		overwrite.validateSettings(settings);
-		outPath.validateSettings(settings);
-		variableParams.validateSettings(settings);
-		creatorGivenName.validateSettings(settings);
-		creatorFamilyName.validateSettings(settings);
-		creatorContact.validateSettings(settings);
-		createdDate.validateSettings(settings);
-		modifiedDate.validateSettings(settings);
-		reference.validateSettings(settings);
+	protected void validateSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
+		// Does nothing
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	protected void loadInternals(final File internDir,
-			final ExecutionMonitor exec) throws IOException,
-			CanceledExecutionException {
+	protected void loadInternals(final File internDir, final ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	protected void saveInternals(final File internDir,
-			final ExecutionMonitor exec) throws IOException,
-			CanceledExecutionException {
+	protected void saveInternals(final File internDir, final ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
 	}
-
-	private static Date getDate(SettingsModelDate date) {
-		return date.getSelectedFields() != 0 ? date.getDate() : null;
-	}
-
 }
